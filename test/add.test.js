b193d7e547b1ec72328d9e696371781e8d1b45d0
Ahmad-Imam <ahmadimam@iut-dhaka.edu>
2023-12-08 18:07:46 +0200

    added all files

--- test/add.test.js
+++ test/add.test.js
@@ -1,12 +1,7 @@
-// const puppeteer = require("puppeteer");
-// const chai = require("chai");
 import chai from "chai";
-const expect = chai.expect;
-// const chaiHttp = require("chai-http");
-import chaiHttp from "chai-http";
-chai.use(chaiHttp);
-
 import add from "../src/add.js";
+
+const expect = chai.expect;
 
 describe("add function", () => {
   it("adds 6 and 4 to equal 10", () => {
--- test/camelCase.test.js
+++ test/camelCase.test.js
@@ -1,25 +1,15 @@
 import chai from "chai";
-const expect = chai.expect;
-import chaiHttp from "chai-http";
-chai.use(chaiHttp);
-
 import camelCase from "../src/camelCase.js";
+
+const expect = chai.expect;
 
 describe("camelCase function", () => {
   it("should convert a simple string to camelCase", () => {
     const result = camelCase("hello world");
     expect(result).to.equal("helloWorld");
   });
-  //   it("should convert a simple string to camelCase", () => {
-  //     const result = camelCase("hello");
-  //     expect(result).to.equal("hello");
-  //   });
-  //   it("should convert a simple string to camelCase", () => {
-  //     const result = camelCase("Foo Bar");
-  //     expect(result).to.equal("fooBar");
-  //   });
-  //   it("should convert a simple string to camelCase", () => {
-  //     const result = camelCase("--foo-bar-");
-  //     expect(result).to.equal("fooBar");
-  //   });
+  it("should remain same for a camelCase string", () => {
+    const result = camelCase("camelCase");
+    expect(result).to.equal("camelCase");
+  });
 });
